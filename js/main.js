b04914d54e7b966d2268b2b480e90ecccb227597
alkaraki998 <51075731+alkaraki998@users.noreply.github.com>
2019-06-25 12:11:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+//fun mul mult 2 numbers and give the result
+function mul() {
+var f1 =window.n1.value;
+var f2 =window.n2.value;
+var re = f1 * f2;
+window.h1.innerHTML=re;
+}
+
+//fun sub sub 2 numbers and give the result
+function sub() {
+    var f1 =window.n1.value;
+    var f2 =window.n2.value;
+    var re = f1 - f2;
+    window.h1.innerHTML=re;
+}
+//fun sum add 2 numbers and give the result
+function sum() {
+    var f1 =window.n1.value;
+    var f2 =window.n2.value;
+    var re = f1 + f2;
+    window.h1.innerHTML=re;
+
+}
+
+//fun div div 2 numbers and give the result
+function div () {
+    var f1 =window.n1.value;
+    var f2 =window.n2.value;
+    var re = f1 / f2;
+    window.h1.innerHTML=re;
+
+
+}
